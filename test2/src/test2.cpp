07dc75434880c5b23726d1459758ce0190111990
Shahaf Dan <dan.shachaf@gmail.com>
2019-08-20 16:37:18 -0700

    added the text file from assignment 0.1

---
+++ test2/src/test2.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : test2.cpp
+// Author      : ShahafDan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int i = 4; i <5 ; i++)
+	{
+		cout << "Hello World " << i << endl;
+	}
+	return 0;
+}
