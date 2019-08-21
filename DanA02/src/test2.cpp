f08590f5151ecb4bd1c7f3d435098bec9dd9b00a
Shahaf Dan <dan.shachaf@gmail.com>
2019-08-20 17:01:12 -0700

    finished project #0.2

---
+++ DanA02/src/test2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DanA02.cpp
+// Author      : ShahafDan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " This is assignment 0.2 of our class (CS20). Join the Chess Club !!!!" << endl;
+}
