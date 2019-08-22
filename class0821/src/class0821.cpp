70e7aae5e3bf5ce25f03a29e41309a8ed5d8b165
Shahaf Dan <dan.shachaf@gmail.com>
2019-08-21 19:34:20 -0700

    started class0821

---
+++ class0821/src/class0821.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : class0821.cpp
+// Author      : ShahafDan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i;
+	int j;
+	cout << "address of i: " << &i << endl;
+	cout << "address of j: " << &j << endl;
+
+}
