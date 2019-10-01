2fdc7b93f6ba8f1c17c04a73d13ef383051d0f30
Shahaf Dan <dan.shachaf@gmail.com>
2019-09-30 19:28:40 -0700

    started assignment 05

---
+++ Assignment_05/src/Assignment_05.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Assignment_05.cpp
+// Author      : ShahafDan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello" << endl << "Please enter the size of your array" << endl;
+	return 0;
+}
