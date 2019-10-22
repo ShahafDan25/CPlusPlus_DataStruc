1be00fd028dbf0ec6f3df33290988989644dec89
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-21 19:26:34 -0700

    finished bubblesort

--- class1021/src/class1021.cpp
+++ class1021/src/class1021.cpp
@@ -12,23 +12,64 @@
 using namespace std;
 
 //new terms
-//*** instrumentation:
-
+//*** instrumentation: counting and keeping track of traversing and looping
+//*** binary serach requires that the array is sorted
+//average time for sorting is O(n^2)
 
 
 //linear search algorithm
-int linearSearch(int array [], int high, int target, int numIter)
+int linearSearch(int array [], int high, int target, int &numIter)
 {
+	int i = 0;
+	bool found = false; // because we have not found the item yet
+	while( i < high && !found)
+	{
+		numIter++;
+		if(array[i] == target) found = true;
+		else i++;
+	}
 
-	return 0;
+	if(found) return i;
+	else return -1;
+}
+
+//bubbleSort algorithm
+void bubbleSort(int array[], int high, int &numIter)
+{
+	int i,j,temp;
+	for(i = 0 ;i < high; i++)
+	{
+		for(j = 0; j < high; j++)
+		{
+			numIter++;
+			if(array[j] > array[j+1])
+			{
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
+
+	for(i = 0; i <= high; i++)
+	{
+		cout << array[i] << endl;
+	}
 }
 
 int main()
 {
-	cout << "code from class October 21st" << endl;
+	cout << "code from class October 21st" << endl << "---------------------------------" << endl << endl;
 	int array [12] = {5,2,9,4,1,0,11,10,12,3,6,7}; //rendomly chosen integerss
-
-
-
+	int numIter = 0;
+	cout << "i = " << linearSearch(array, 11, 10, numIter) << endl;  //pass the  array, with 11+1 elements, looking for ten, passing the instrumentation variable
+	cout << "numIter = " << numIter << endl;
+	//now calling the bubble sort function
+	cout << endl << endl;
+	numIter = 0;
+	bubbleSort(array, 11, numIter);
+	cout << "numIter = " << numIter << endl;
+	cout << endl << endl;
+	//now running binary search
 	return 0;
 }
