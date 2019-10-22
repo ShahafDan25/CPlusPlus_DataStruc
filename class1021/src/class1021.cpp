d2c518d6743457d6214fa2d9413304920171513b
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-21 18:55:08 -0700

    started class1021

---
+++ class1021/src/class1021.cpp
@@ -0,0 +1,34 @@
+//============================================================================
+// Name        : class1021.cpp
+// Author      : ShahafDan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+//new terms
+//*** instrumentation:
+
+
+
+//linear search algorithm
+int linearSearch(int array [], int high, int target, int numIter)
+{
+
+	return 0;
+}
+
+int main()
+{
+	cout << "code from class October 21st" << endl;
+	int array [12] = {5,2,9,4,1,0,11,10,12,3,6,7}; //rendomly chosen integerss
+
+
+
+	return 0;
+}
