5eb6ace1fec186df33202cc3cd5cfc75c86038f5
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-23 00:40:16 -0700

    started a6

--- Assignment_06/src/Assignment_06.cpp
+++ Assignment_06/src/Assignment_06.cpp
@@ -9,32 +9,50 @@
 #include "Q.hpp"
 #include <iostream>
 #include <iomanip>
+#include <time>
 using namespace std;
-
+bool checkProb (int prob)
+{
+	//generate a random number
+	//check for validity
+	int rand = rand() % 100 + 1;
+	if(rand() <= prob) return true;
+	else return false;
+}
 
 int main()
 {
-	int size, clock, ticks, count;
-	server * s = new server;
-	count = size - 1;
+	int size, clock, ticks, newTransProb, endTransProb;
 	cout << "Please enter Queue size" << endl;
 	cin >>  size;
-	Q(size);
+	Q * q = new Q(size);
 
-	s -> busy = false;
+	cout << "How many clock ticks are we looking at?" << endl;
+	cin >> ticks;
+
+	cout << "what is the success probability? number between 1 and 100" << endl;
+	cin >>  newTransProb;
+	while(newTransProb < 1 || newTransProb > 100)
+	{
+		cout << "Invalid, enter probability again (percentage)" << endl;
+		cin >> newTransProb;
+	}
+
+	cout << "what is the failure probability? number between 1 and 100" << endl;
+	cin >>  endTransProb;
+	while(endTransProb < 1 || endTransProb > 100)
+	{
+		cout << "Invalid, enter probability again (percentage)" << endl;
+		cin >> endTransProb;
+	}
 	clock = 0;
 	while(clock < ticks)
 	{
-		if(!s->busy)
-		{
-			if(count > 0)
-			{
+		// ------ ARRIVAL -------
 
-				count --;
-				s -> busy = true;
-			}
-		}
-
+		// ------ PROCESSING -----
+		// ------ LEAVING ------
+		clock++;
 	}
 	return 0;
 }
