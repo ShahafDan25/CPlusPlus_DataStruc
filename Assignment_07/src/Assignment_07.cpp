ec5441fcb5b99a47d62ea6873a81969dab5aa3ed
Shahaf Dan <dan.shachaf@gmail.com>
2019-10-26 15:36:53 -0700

    started a7

---
+++ Assignment_07/src/Assignment_07.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Assignment_07.cpp
+// Author      : ShahafDan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "WELCOME TO ASSIGNMENT 7 BY SHAHAF DAN" << endl;
+	return 0;
+}
