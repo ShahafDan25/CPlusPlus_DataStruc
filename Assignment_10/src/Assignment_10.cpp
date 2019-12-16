74e8c37d081f7fea7b0db75071ee7fc072017353
Shahaf Dan <dan.shachaf@gmail.com>
2019-12-16 00:35:15 -0800

    fixed BFT

--- Assignment_10/src/Assignment_10.cpp
+++ Assignment_10/src/Assignment_10.cpp
@@ -1,5 +1,4 @@
-
-	//============================================================================
+//============================================================================
 // Name        : Assignment_10.cpp
 // Author      : ShahafDan
 // Version     :
@@ -22,7 +21,7 @@ struct Vertex
 {
 	int value;
 	bool visited;
-	vector <int> neighbors;
+	vector <Vertex * > neighbors;
 };
 
 
@@ -31,17 +30,6 @@ void bft (vector <Vertex*>  &graph);
 void dft (vector <Vertex*> & graph);
 void dftInternal(vector <Vertex *> & graph, Vertex *& v);
 void buildGraph(vector <Vertex*> & graph);
-int lookup(vector <Vertex *> graph, int value);
-
-int lookup(vector <Vertex *> graph, int numeric)
-{
-	for(int i = 0; i < graph.size(); i++)
-	{
-		if(graph[i] -> value == numeric) return i;
-	}
-	return -1;
-}
-
 
 void buildGraph(vector <Vertex*> & graph)
 {
@@ -59,40 +47,68 @@ void buildGraph(vector <Vertex*> & graph)
 	{
 		cout << "SUCCESSFULLY OPENED DATA FILE" << endl << endl;
 		cout << " ** HERE IS OUR GRAPH: " << endl << endl;;
-		Vertex * v; //for building purposes
-
+		Vertex * v = new Vertex;
+		int counter = 0;
+		dataFile >> curNum;
+		v -> value = curNum;
+		v -> visited = false;
+		bool found;
+		//graph.push_back(v); //add to the graph the first vertex (0)
+		Vertex * vN = new Vertex;
 	////// STAGE II : BUILD THE GRAPH
 		while(!dataFile.eof())
-		{
-			v = new Vertex; //so it will not override each vertex in the graph
-			dataFile >> curNum;
-			if(curNum == -1) break;
-			v -> value = curNum;
-			while(!dataFile.eof())
-			{
-				dataFile >> curNum;
-				if(curNum == -1) break;
-				v -> neighbors.push_back(curNum);
-			}
-			graph.push_back(v);
-		}
+				{
+					dataFile >> curNum;
+					if(curNum == -1) //list of neighbors over
+					{
+						graph.push_back(v);
+						v = new Vertex;
+						counter++;
+						dataFile >> curNum;
+						v -> value = curNum;
+						v -> visited = false;
+					}
+					else
+					{
+						Vertex * vNeigh = new Vertex;
+						vNeigh -> visited = false;
+						vNeigh -> value = curNum;
+						v -> neighbors.push_back(vNeigh);
+					}
 
-		//// STAGE III: PRINT OUT THE GRAPH
-		for(int k = 0; k < graph.size(); k++)
-		{
-			cout << "Vertex: " << k << ".   Value: " << graph[k] -> value << ".   Neightbors -> ";
-			//loop to print neighbors
-			for(int i = 0; i < graph[k] -> neighbors.size(); i++)
-			{
-				cout << graph[k] -> neighbors[i] << " ";
-			}
-			cout << endl;
-		}
+				}
 		delete v;					//get rid of what we dynamically allocated
 	} // end else
 
+	// STAGE II.V : RE EDIT THE GRAPH TO MATCH WITH POINTERS
+	Vertex * vT = new Vertex;
+	for(int i = 0; i < graph.size() - 1; i++)
+	{
+		vT = new Vertex;
+		for(int k = 0; k < graph[i] -> neighbors.size(); k++)
+		{
+			for(int l = i; l < graph.size(); l++)
+			{
+				if(graph[i] -> neighbors[k] -> value == graph [l] -> value)
+				{
+					graph[i] -> neighbors[k] = graph[l];
+				}
+			}
+		}
+	}
 
+	/// STAGE III: PRINT GRAPH
+	for (int i = 0 ; i < graph.size(); i++)
+	{
+		cout << "vertex number " << i << ", value: " << graph[i] -> value << ", neighbors -> ";
+		for(int j = 0; j < graph[i] -> neighbors.size(); j++ ) //traverse through neighbors in the vertex's vectors of graph
+		{
+			cout << graph[i] -> neighbors[j] -> value << "  ";
+		}
+		cout << endl;
+	} //the first vertex was inserted twice (find out why?) TODO
 
+	delete vT; //finished building graph correctly
 	return;
 }
 
@@ -101,13 +117,48 @@ int counter;
 void dft (vector <Vertex*> & graph)
 {
 
-
+	counter = 0; // becasue the first one is inserted twice
+	for(int i = 0; i < graph.size(); i++) //begin by setting all of the visited nodes to 0
+	{
+		graph[i] -> visited = false;
+	}
+	Vertex * v = new Vertex;
+	v = graph [counter];
+	while(counter < graph.size())
+	{
+		if(v -> visited == false) dftInternal (graph, v); // quitting here for some reason, check internal function
+		v = graph[++counter];
+	}	// end while
+	delete v;	//delete the trav vertex we dynamically allocated
+	return;
 }
 int numerator;
 
 void dftInternal(vector <Vertex *> & graph, Vertex *& v) //pass by reference the vertex so we can change its "visited" value
 {
-
+	if(v -> visited == false)
+	{
+		cout << v -> value << "\t";
+	}
+	v -> visited = true;
+	numerator = 0;
+	while(numerator < v -> neighbors.size())
+	{
+ 		if(v -> neighbors[numerator] -> visited == false)
+ 		{
+ 			if(v -> neighbors[numerator] -> neighbors.size() > 0)
+ 			{
+ 				dftInternal(graph, v -> neighbors[numerator]);
+ 				numerator++;
+ 			}
+ 			else
+ 			{
+ 				v -> neighbors[numerator] -> visited = true;
+ 				cout << v -> neighbors[numerator] -> value << ", ";
+ 				return;
+ 			}
+ 		}
+	}
 	return;
 }
 
@@ -115,36 +166,47 @@ void dftInternal(vector <Vertex *> & graph, Vertex *& v) //pass by reference the
 //*******************     BREADTH - FIRST TRAVERSAL      *****************************//
 void bft (vector <Vertex*> & graph)
 {
+	Vertex * curV = new Vertex;	//curv for current vertex
+	Vertex * curvTemp = new Vertex;
 	queue <Vertex *> vs; // create a queue of pointers to vertices calles vs
-	graph[0] -> visited = true;
-	cout << graph[0] -> value << ", ";
-	cout << graph[0] -> value << ", ";
-	vs.push(graph[0]); ///push the first element
-	Vertex * temp = new Vertex;
-	Vertex * curv = new Vertex;
-	temp = vs.front();
-	while(!vs.empty())
+	for(int i = 0; i < graph.size(); i++)
 	{
-		for(int i = 0; i < temp -> neighbors.size(); i++)
-		{
-			curv = graph[lookup(graph, temp -> neighbors[i])];
-			if(curv -> visited == false)
-			{
-				curv -> visited = true;
-				cout << curv -> value << ", ";
-				vs.push(curv);
-			}
-		}
-		vs.pop();
+		graph[i] -> visited = false;	//setting all vreteices to unvisited
 	}
+
+
 	for(int i = 0; i < graph.size(); i++)
 	{
 		if(graph[i] -> visited == false)
 		{
 			graph[i] -> visited = true;
-			cout << graph[i] -> value << ", ";
+			vs.push(graph[i]);
+		}
+		while(!vs.empty())
+		{
+			curV = vs.front();
+			vs.pop();
+			cout << curV -> value << ", ";
+			for(int j = 0; j < curV -> neighbors.size(); j++)
+			{
+				curvTemp = curV -> neighbors[j];
+				for(int k = 0; k < graph.size(); k++)
+				{
+					if(curvTemp -> value == graph[k] -> value)
+					{
+						if(graph[k] -> visited == false)
+						{
+							graph[k] -> visited = true;
+							vs.push(graph[k]);
+						}
+					}
+				}
+			}
 		}
 	}
+
+
+
 	return;
 }// end functions
 
@@ -163,3 +225,5 @@ int main() {
 	cout << endl <<  "Have a great day! TAF" << endl;
 	return 0;
 }
+
+
